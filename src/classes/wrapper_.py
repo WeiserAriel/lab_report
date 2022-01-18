3c4cf9c7b5517c0dde0a5bce1567f97e51c2832c
WeiserAriel <weiser.ariel@gmail.com>
2022-01-18 12:19:09 +0200

    new changes for lab report #32

--- src/classes/wrapper_.py
+++ src/classes/wrapper_.py
@@ -110,62 +110,72 @@ class Wapper():
     @staticmethod
     def Create_devices_objects(device_list_ip):
         #Create tempory SSH connection to be able to create device list:
-        logging.debug("connecting to r-ufm34 to create device list")
-        dev = Device('10.209.36.92','smg-ib-jenkins02','linux_host' ,'root', '3tango',None,'Nobody')
+        main_device = 'smg-ib-jenkins02'
+        main_device_ip = '10.209.27.125'
+        logging.debug("connecting to " + main_device+ " to create device list")
+        try:
+            dev = Device(main_device_ip, main_device,'linux_host' ,'root', '3tango',None,'Nobody')
+            if dev.ssh_client == None:
+                logging.error('Couldnt create SSH connection for main device : ' + main_device + ' Exit script ')
+                exit(-1)
+        except Exception as e:
+            logging.error('Exception in Creating mian device object : ' + main_device + ' ' + str(e))
         device_list = []
-
-        for device in device_list_ip.keys():
-            logging.debug(" start Creating device object for :" + device)
-            owner = device_list_ip[device]
-            #identify from DHCP what type of device is it:
-            logging.debug("running cmd : " + 'cat /auto/LIT/SCRIPTS/DHCPD/list | grep -i ' + device)
-            cmd = 'cat /auto/LIT/SCRIPTS/DHCPD/list | grep -i ' + device
-            out = dev.run_command(cmd)
-            #if device has no next server it means that it was not found in DHCP
-            if 'next-server' in out:
-                logging.debug("device exist in dhcp : " + device)
-                #TODO - need to check which row starts with IP
-                rows = out.split('\n')
-                for row in rows:
-                    regex = '\d{1,3}\.{1}\d{1,3}\.{1}\d{1,3}\.\d{1,3}.*next-server'
-                    found = Device.search_in_regex(row, regex)
-                    if found and (row.split('; ')[2] == device):
-                        device_name = str(row.split(';')[2]).replace(" ","")
-                        device_ip = str(row.split(';')[0]).replace(" ","")
-                        break
-                else:
-                    logging.debug("couldn't find device name and device ip according to DHCP output for device : "  + device)
-                    logging.debug("out  : " + out)
-                    logging.debug("regex : " + regex)
-                    logging.debug("Continue to the next device... ")
-                    continue
-                if device_name in Constants.ignore_devices:
-                    continue
-                elif 'apl' in row:
-                    #check if this is gen2 or gen1
-                    logging.debug("device identiry as appliance : " + device_name)
-                    if 'gen1' in row:
-                        tmp_device = Apl_Host(device_ip, device_name, 'GEN1', dev,owner)
-                    elif 'gen25' in row:
-                        tmp_device = Apl_Host(device_ip, device_name, 'GEN2.5', dev, owner)
-                    elif 'gen2' in row:
-                        tmp_device = Apl_Host(device_ip, device_name, 'GEN2', dev, owner)
-                    elif 'gen3' in row:
-                        tmp_device = Apl_Host(device_ip, device_name, 'GEN3', dev,owner)
-                    elif 'gen4' in row:
-                        tmp_device = Apl_Host(device_ip, device_name, 'GEN4', dev,owner)
+        try:
+            for device in device_list_ip.keys():
+                logging.debug(" start Creating device object for :" + device)
+                owner = device_list_ip[device]
+                #identify from DHCP what type of device is it:
+                logging.debug("running cmd : " + 'cat /auto/LIT/SCRIPTS/DHCPD/list | grep -i ' + device)
+                cmd = 'cat /auto/LIT/SCRIPTS/DHCPD/list | grep -i ' + device
+                out = dev.run_command(cmd)
+                #if device has no next server it means that it was not found in DHCP
+                if 'next-server' in out:
+                    logging.debug("device exist in dhcp : " + device)
+                    #TODO - need to check which row starts with IP
+                    rows = out.split('\n')
+                    for row in rows:
+                        regex = '\d{1,3}\.{1}\d{1,3}\.{1}\d{1,3}\.\d{1,3}.*next-server'
+                        found = Device.search_in_regex(row, regex)
+                        if found and (row.split('; ')[2] == device):
+                            device_name = str(row.split(';')[2]).replace(" ","")
+                            device_ip = str(row.split(';')[0]).replace(" ","")
+                            break
                     else:
-                        logging.error('Couldn\'t recognize the generation of the ufm appliance ' + str(device_name))
-                elif 'sw' in row or 'gw' in row or 'olg' in row:
-                    logging.debug("device identify as switch : " + device_name)
-                    tmp_device = Switch(device_ip, device_name, 'switch',dev,owner)
+                        logging.debug("couldn't find device name and device ip according to DHCP output for device : "  + device)
+                        logging.debug("out  : " + out)
+                        logging.debug("regex : " + regex)
+                        logging.debug("Continue to the next device... ")
+                        continue
+                    if device_name in Constants.ignore_devices:
+                        continue
+                    elif 'apl' in row:
+                        #check if this is gen2 or gen1
+                        logging.debug("device identiry as appliance : " + device_name)
+                        if 'gen1' in row:
+                            tmp_device = Apl_Host(device_ip, device_name, 'GEN1', dev,owner)
+                        elif 'gen25' in row:
+                            tmp_device = Apl_Host(device_ip, device_name, 'GEN2.5', dev, owner)
+                        elif 'gen2' in row:
+                            tmp_device = Apl_Host(device_ip, device_name, 'GEN2', dev, owner)
+                        elif 'gen3' in row:
+                            tmp_device = Apl_Host(device_ip, device_name, 'GEN3', dev,owner)
+                        elif 'gen4' in row:
+                            tmp_device = Apl_Host(device_ip, device_name, 'GEN4', dev,owner)
+                        else:
+                            logging.error('Couldn\'t recognize the generation of the ufm appliance ' + str(device_name))
+                    elif 'sw' in row or 'gw' in row or 'olg' in row:
+                        logging.debug("device identify as switch : " + device_name)
+                        tmp_device = Switch(device_ip, device_name, 'switch',dev,owner)
+                    else:
+                        logging.debug("device identify as linux host : " + device_name)
+                        tmp_device = Linux_Host(device_ip, device_name,'linux_host', dev,owner)
+                    logging.debug("append deivice after creation to device list : " + device_name)
+                    device_list.append(tmp_device)
                 else:
-                    logging.debug("device identify as linux host : " + device_name)
-                    tmp_device = Linux_Host(device_ip, device_name,'linux_host', dev,owner)
-                logging.debug("append deivice after creation to device list : " + device_name)
-                device_list.append(tmp_device)
-            else:
-                logging.debug("device not exist in dhcp : " + device + ", device will not be added to device list")
+                    logging.debug("device not exist in dhcp : " + device + ", device will not be added to device list")
+        except Exception as e:
+            logging.error('Exception in Create_devices_objects for device : ' + device)
         return device_list
 
             #Print the results from the container.
